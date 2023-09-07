e3f94935d072c0dd3667b5745029ee152cdb7558
kenichikawaguchi <xianzaikaishiba@gmail.com>
2023-09-07 23:39:20 +0900

    Add login

--- portal/accounts/urls.py
+++ portal/accounts/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+from django.contrib.auth import views as auth_views
+
+
 app_name = 'accounts'
 
 urlpatterns = [
@@ -11,4 +14,12 @@ urlpatterns = [
     path('signup_success/',
         views.SignUpSuccessView.as_view(),
         name='signup_success'),
+    path('login/',
+        auth_views.LoginView.as_view(template_name="login.html"),
+        name='login'
+        ),
+    path('logout/',
+        auth_views.LogoutView.as_view(template_name="logout.html"),
+        name='logout'
+        ),
 ]
