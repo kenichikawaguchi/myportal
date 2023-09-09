905345eae750b8ff59cdf1045e7672ad514107a1
Kenichi Kawaguchi <sonatine@pontalk.com>
2023-09-09 14:37:48 +0900

    email settings

--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -134,3 +134,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+DEFAULT_FROM_EMAIL = env.get_value('DEFAULT_FROM_EMAIL')
+EMAIL_HOST = env.get_value('EMAIL_HOST')
+EMAIL_PORT = int(env.get_value('EMAIL_PORT'))
+EMAIL_HOST_USER = env.get_value('EMAL_HOST_USER')
+EMAIL_HOST_PASSWORD = env.get_value('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = env.get_value('EMAIL_USE_TLS')
