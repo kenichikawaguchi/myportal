58819f74d4196fddf5ffb39f5710397c9e03f75d
Kenichi Kawaguchi <sonatine@pontalk.com>
2023-09-09 09:20:57 +0900

    password_reset

--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -120,7 +120,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files ( CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
@@ -133,3 +133,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
